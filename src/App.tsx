a6db7e64d31d260466a44d5d205c8f2c665a3181
Yash Patel <yashpatel02010@gmail.com>
2026-06-09 23:49:15 +0530

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
@@ -43,6 +44,7 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
+        <Analytics />
         </BrowserRouter>
       </TooltipProvider>
     </ThemeProvider>
